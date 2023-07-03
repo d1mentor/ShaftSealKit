f9fef5cd89f82a5576a74bb59085e38fd3aaef0d
d1mentor <gerasimenkot92@gmail.com>
2023-07-03 21:53:25 +0300

    add banlist

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,15 @@
 class ContactMailer < ApplicationMailer
     default from: "gerasimenkot92@gmail.com"
     
+    banlist = ['Robertgraib']
+
     def send_email(name, email, message)
       @name = name
       @message = message
       @email = email
-      mail(to: 'shaftsealkit@gmail.com', subject: 'Запрос с вэдмэдыка')
+      if !banlist.include?(@name)
+        mail(to: 'shaftsealkit@gmail.com', subject: 'Запрос с вэдмэдыка')
+      end
     end
   end
   
